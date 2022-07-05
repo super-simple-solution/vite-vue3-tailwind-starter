ac3da80b00e00bb67b7f97cd92f5f20d4068dd28
zhangpan <zhangpanrobot@gmail.com>
2022-07-05 16:52:39 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,12 +4,8 @@ import ButtonRepo from '@/components/ButtonRepo.vue'
 
 <template>
   <div class="bg-gray-50">
-    <div
-      class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8"
-    >
-      <h2
-        class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10"
-      >
+    <div class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
+      <h2 class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
         Ready to dive in?
         <br />
         <span class="text-indigo-600">Vite + Vue 3 + Tailwind CSS</span>
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,57 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import Components from "unplugin-vue-components/vite"
+import AutoImport from "unplugin-auto-import/vite"
+import { resolve } from "path"
+import autoprefixer from "autoprefixer"
+import eslintPlugin from "vite-plugin-eslint"
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import tailwind from 'tailwindcss'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      dts: true,
+      dirs: ["src/components"],
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: "sass",
+        }),
+      ],
+    }),
+    eslintPlugin(),
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+        /\.json$/, // .json
+      ],
+      // global imports to register
+      imports: [
+        // presets
+        "vue",
+      ],
+      eslintrc: {
+        enabled: true, // Default `false`
+        filepath: "./.eslintrc-auto-import.json", // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      },
+      resolvers: [() => null],
+      dts: "./auto-imports.d.ts",
+    }),
+  ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
     },
   },
   server: {
